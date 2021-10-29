76f52fad54d768d84d1e7b979c53a7bb365c89c2
ravitejav <raviteja99866@gmail.com>
2021-10-29 20:31:19 +0530

    added intro page

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,23 +1,11 @@
-import './Home.css';
-// import { NavBar } from "../NavBar";
-import { UserProfile} from "../UserProfile";
-import { Skills } from '../UserProfile/Skills';
-import { Experience } from '../Experience';
-import { Projects } from '../Projects';
-import { SocialMediaProfiles } from '../SocialMediaProfiles';
-
+import { Intro } from "Components/Intro";
 
 const Home = () => {
     return (
-        <main className="HomepageWrapper">
-            {/* <NavBar /> */}
-            <UserProfile />
-            <Skills />
-            <Experience />
-            <Projects />
-            <SocialMediaProfiles />
+        <main style={{ backgroundColor: '#232323' }}>
+            <Intro />
         </main>
-    )
+    );
 }
 
 export default Home;
