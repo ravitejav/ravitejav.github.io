e726777b037c8c42e7d8bae226718c63d914221a
ravitejav <raviteja99866@gmail.com>
2022-05-27 07:41:54 +0530

    changes experince

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -16,7 +16,7 @@ export const Intro = () => {
                 <p className="greeting">Hi, my name is,</p>
                 <span className="tintLine"></span>
                 <span className="name">Raviteja.</span>
-                <p className="introInfo">I am working as an Application Developer 3 at Oracle with an overall Experience of 2.5+ years experience in FrontEnd Applications development. I'm Skilled in ReactJS, React Native, Angular, Javascript, Java and Spring Boot. Looking forward to working in a challenging environment.</p>
+                <p className="introInfo">I am working as an Application Developer 3 at Oracle with an overall Experience of 3+ years experience in FrontEnd Applications development. I'm Skilled in ReactJS, React Native, Angular, Javascript, Java and Spring Boot. Looking forward to working in a challenging environment.</p>
                 <div className="links">
                     <a href="mailto:raviteja99866@gmail.com" className="mail center">
                         <FontAwesomeIcon icon={faEnvelope} />
